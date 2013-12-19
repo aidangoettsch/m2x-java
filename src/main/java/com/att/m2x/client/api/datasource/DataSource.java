82e6df5d09c4b9f9a3e2c830d5fca50c91dcf8f2
Aidan <yayestechlab@me.com>
2013-12-19 15:46:39 -0500

    Fix #3

--- src/main/java/com/att/m2x/client/api/datasource/DataSource.java
+++ src/main/java/com/att/m2x/client/api/datasource/DataSource.java
@@ -4,9 +4,14 @@ package com.att.m2x.client.api.datasource;
 public class DataSource extends CommonDataSource {
 
     private String batch;
+    private String batch_name;
 
     public String getBatch() {
         return batch;
+    }
+    
+    public String getBatchName() {
+        return batch_name;
     }
 
 }
